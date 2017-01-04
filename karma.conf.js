d0b9b67622503d06c0ad6db84a85b0c0ec36ed5b
extraErik <erygg01@live.com>
2017-01-04 09:36:22 -0600

    Added test coverage

--- karma.conf.js
+++ karma.conf.js
@@ -12,16 +12,58 @@ module.exports = function (config) {
         ],
         preprocessors: {
             'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+            // 'app/app.jsx': ['webpack', 'sourcemap', 'coverage'],
+            // 'app/components/*.jsx': ['webpack', 'sourcemap', 'coverage']
         },
-        reporters: ['mocha'],
+        reporters: ['mocha', 'coverage'],
         client: {
             mocha: {
                 timeout: '5000'
             }
         },
-        webpack: webpackConfig,
+        //webpack: webpackConfig,
+        webpack: { //kind of a copy of your webpack config
+              devtool: 'inline-source-map', //just do inline source maps instead of the default
+              resolve: {
+                  root: __dirname,
+                  modulesDirectories: [
+                      'node_modules',
+                      './app/components',
+                      './app/api'
+                  ],
+                  alias: {
+                      app: 'app',
+                      applicationStyles: 'app/styles/app.scss'
+                  },
+                  extensions: ['', '.js', '.jsx']
+              },
+              module: {
+                loaders: [
+                  {
+                      loader: 'babel-loader',
+                      query: {
+                          presets: ['react', 'es2015', 'stage-0']
+                      },
+                      test: /\.jsx?$/,
+                      exclude: /(node_modules|bower_components)/
+                  }
+                ],
+                postLoaders: [
+                    { //delays coverage til after tests are run, fixing transpiled source coverage error
+                        test: /\.jsx?$/,
+                        exclude: /(test|node_modules|bower_components)\//,
+                        loader: 'istanbul-instrumenter'
+                    }
+                ]
+            }
+        },
+
         webpackServer: {
             noInfo: true
+        },
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
         }
     });
 };
